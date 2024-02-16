5b704aa55c26fa8f9a68ff56c809719efcf2e094
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2024-02-16 15:57:47 -0500

    fix stdout

--- src/tests/tester.py
+++ src/tests/tester.py
@@ -1,32 +1,27 @@
 import subprocess
 import csv
 
-"""
 datafile = open("data.csv", "w")
 print(
-    "Test, Original Code Output, 2to3 Output, 2to3 Output Matches Original Output, 2to3 Run Time, fix_print Output, fix_print Output Matches Original Output, fix_print Run Time",
+    "Test | Original Code Output | 2to3 Translation | 2to3 Output | 2to3 Output Matches Original Output | 2to3 Run Time | fix_print Translation | fix_print Output | fix_print Output Matches Original Output | fix_print Run Time",
     file=datafile,
 )
-"""
 
 for i in range(1, 4):
     two_to_three_test = subprocess.run(
         ["2to3", f"test{i}.py2"], capture_output=True, encoding="utf-8"
     )
 
-    result = two_to_three_test.stdout.splitlines()
-    for line in result:
+    two_to_three_test_result = two_to_three_test.stdout.splitlines()
+    for line in two_to_three_test_result:
         if line.startswith("+"):
             two_to_three_translation = line.lstrip("+")
 
-    print(f"{two_to_three_translation=}")
+    two_to_three_translation_output = subprocess.run(
+        ["python", "-c", two_to_three_translation], capture_output=True
+    ).stdout
 
-    test2 = subprocess.run(
-        ["python", "-c", f"'{two_to_three_translation}'"],
-        capture_output=True,
-        encoding="utf-8",
+    print(
+        f"{i} | {two_to_three_translation} | {two_to_three_translation_output}",
+        file=datafile,
     )
-
-    print(f"{test2.stdout=}")
-
-    # print(f"{i},", file=datafile)
