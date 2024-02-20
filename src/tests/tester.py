b5d6983e3bf3587fd8b3b48ab7386e91d3c58b68
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2024-02-20 00:57:06 -0500

    tests work

--- src/tests/tester.py
+++ src/tests/tester.py
@@ -6,17 +6,39 @@ print(
     file=datafile,
 )
 
+tests = [
+    "test_1",
+    "test_2",
+    "test_3",
+    "test_prefix_preservation",
+    "test_trailing_comma_1",
+    "test_trailing_comma_2",
+    "test_trailing_comma_3",
+    "test_tuple",
+]
 
-for i in range(1, 12):
+idempotency_tests = [
+    "test_idempotency-1",
+    "test_idempotency-2",
+]
+
+file_redirection_tests = [
+    "test_vargs_without_trailing_comma",
+    "test_with_trailing_comma",
+    "test_no_trailing_comma",
+    "test_spaces_before_file",
+]
+
+for test in tests:
     two_to_three_runtimes = []
 
     for j in range(5):
         python_two_output = subprocess.run(
-            ["python2", f"test{i}.py2"], capture_output=True
+            ["python2", test], capture_output=True
         ).stdout
 
         two_to_three_test = subprocess.run(
-            f"time 2to3 test{i}.py2",
+            f"time 2to3 {test}",
             shell=True,
             executable="/bin/bash",
             capture_output=True,
@@ -42,22 +64,58 @@ for i in range(1, 12):
     )
 
     print(
-        f"{i} | {python_two_output} | {two_to_three_translation} | {two_to_three_translation_output} | {two_to_three_runtimes[0]} | {two_to_three_runtimes[1]} | {two_to_three_runtimes[2]} | {two_to_three_runtimes[3]} | {two_to_three_runtimes[4]} | {two_to_three_average_runtime} | {python_two_output_equals_two_to_three_output}",
+        f"{test} | {python_two_output} | {two_to_three_translation} | {two_to_three_translation_output} | {python_two_output_equals_two_to_three_output} | {two_to_three_runtimes[0]} | {two_to_three_runtimes[1]} | {two_to_three_runtimes[2]} | {two_to_three_runtimes[3]} | {two_to_three_runtimes[4]} | {two_to_three_average_runtime}",
         file=datafile,
     )
 
-
-for i in range(1, 7):
-    subprocess.run(["python2", f"filetest{i}.py2"])
-
-    with open(f"filetest{i}.txt", "br") as two_output_file:
-        python_two_output = two_output_file.read()
-
+for test in idempotency_tests:
     two_to_three_runtimes = []
 
     for j in range(5):
+        python_two_output = subprocess.run(
+            ["python2", test], capture_output=True
+        ).stdout
+
         two_to_three_test = subprocess.run(
-            f"time 2to3 filetest{i}.py2",
+            f"time 2to3 {test}",
+            shell=True,
+            executable="/bin/bash",
+            capture_output=True,
+            encoding="utf-8",
+        )
+
+        with open(test) as test_file:
+            two_to_three_translation = test_file.read()
+
+        for line in two_to_three_test.stderr.splitlines():
+            if line.startswith("real"):
+                two_to_three_runtimes.append(int(line[-4:-1]))
+
+    two_to_three_average_runtime = sum(two_to_three_runtimes) / 5
+
+    two_to_three_translation_output = subprocess.run(
+        ["python", "-c", two_to_three_translation], capture_output=True
+    ).stdout
+
+    python_two_output_equals_two_to_three_output = (
+        python_two_output == two_to_three_translation_output
+    )
+
+    print(
+        f"{test} | {python_two_output} | {two_to_three_translation} | {two_to_three_translation_output} | {python_two_output_equals_two_to_three_output} | {two_to_three_runtimes[0]} | {two_to_three_runtimes[1]} | {two_to_three_runtimes[2]} | {two_to_three_runtimes[3]} | {two_to_three_runtimes[4]} | {two_to_three_average_runtime}",
+        file=datafile,
+    )
+
+for test in file_redirection_tests:
+    two_to_three_runtimes = []
+
+    for j in range(5):
+        python_two_output = subprocess.run(
+            ["python2", test], capture_output=True
+        ).stderr
+
+        two_to_three_test = subprocess.run(
+            f"time 2to3 {test}",
             shell=True,
             executable="/bin/bash",
             capture_output=True,
@@ -74,23 +132,16 @@ for i in range(1, 7):
 
     two_to_three_average_runtime = sum(two_to_three_runtimes) / 5
 
-    subprocess.run(
-        [
-            "python",
-            "-c",
-            'x = open("filetest1.txt", "w")' + "\n" + two_to_three_translation,
-        ],
+    two_to_three_translation_output = subprocess.run(
+        ["python", "-c", "import sys;" + two_to_three_translation],
         capture_output=True,
-    )
-
-    with open(f"filetest{i}.txt", "br") as two_output_file:
-        two_to_three_translation_output = two_output_file.read()
+    ).stderr
 
     python_two_output_equals_two_to_three_output = (
         python_two_output == two_to_three_translation_output
     )
 
     print(
-        f"Filetest {i} | {python_two_output} | {two_to_three_translation} | {two_to_three_translation_output} | {two_to_three_runtimes[0]} | {two_to_three_runtimes[1]} | {two_to_three_runtimes[2]} | {two_to_three_runtimes[3]} | {two_to_three_runtimes[4]} | {two_to_three_average_runtime} | {python_two_output_equals_two_to_three_output}",
+        f"{test} | {python_two_output} | {two_to_three_translation} | {two_to_three_translation_output} | {python_two_output_equals_two_to_three_output} | {two_to_three_runtimes[0]} | {two_to_three_runtimes[1]} | {two_to_three_runtimes[2]} | {two_to_three_runtimes[3]} | {two_to_three_runtimes[4]} | {two_to_three_average_runtime}",
         file=datafile,
     )
