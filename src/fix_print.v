387047056e645223ce9f530685b26113e4e63026
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2024-02-20 00:05:46 -0500

    tests

--- src/fix_print.v
+++ src/fix_print.v
@@ -7,7 +7,7 @@ Require Import Extraction.
 Inductive leaf : Type :=
 | ENDMARKER : leaf
 | NAME : string -> leaf
-| NUMBER : leaf
+| NUMBER : string -> leaf
 | STRING : string -> leaf
 | NEWLINE : leaf
 | INDENT : leaf
@@ -70,86 +70,92 @@ Inductive tree : Type :=
 | LEAF : leaf -> tree
 | NODE : list tree -> tree.
 
-Fixpoint concat_strings (lst : list string) : string :=
-match lst with
+Fixpoint concat_strings (l : list string) : string :=
+match l with
 | [] => "" 
 | x :: xs => x ++ concat_strings xs
 end.
 
-Fixpoint to_string (t : tree) : string :=
-match t with
-| LEAF (NAME n) => n
-(**| LEAF NUMBER => leaf**)
-| LEAF (STRING s) => "'" ++ s ++ "'"
-| LEAF LPAR => "("
-| LEAF RPAR => ")"
-| LEAF COLON => ":"
-| LEAF COMMA => ","
-| LEAF SEMI => ";"
-| LEAF PLUS => "+"
-| LEAF MINUS => "-"
-| LEAF STAR => "*"
-| LEAF SLASH => "/"
-| LEAF AMPER => "&"
-| LEAF LESS => "<"
-| LEAF GREATER => ">"
-| LEAF EQUAL => "="
-| LEAF DOT => "."
-| LEAF PERCENT => "%"
-| LEAF BACKQUOTE => "`"
-| LEAF LBRACE => "{"
-| LEAF RBRACE => "}"
-| LEAF EQEUQAL => "=="
-| LEAF NOTEQUAL => "!="
-| LEAF LESSEQUAL => "<="
-| LEAF GREATEREQUAL => ">="
-| LEAF TILDE => "~"
-| LEAF CIRCUMFLEX => "^"
-| LEAF LEFTSHIFT => "<<"
-| LEAF RIGHTSHIFT => ">>"
-| LEAF DOUBLESTAR => "**"
-| LEAF PLUSEQUAL => "+="
-| LEAF MINEQUAL => "-="
-| LEAF STAREQUAL => "*="
-| LEAF SLASHEQUAL => "/="
-| LEAF PERCENTEQUAL => "%="
-| LEAF AMPEREQUAL => "&="
-| LEAF VBAREQUAL => "|="
-| LEAF CIRCUMFLEXEQUAL => "^="
-| LEAF LEFTSHIFTEQUAL => "<<="
-| LEAF RIGHTSHIFTEQUAL => ">>="
-| LEAF DOUBLESTAREQUAL => "**="
-| LEAF DOUBLESLASH => "//"
-| LEAF DOUBLESLASHEQUAL => "//="
-| LEAF AT => "@"
-| LEAF ATEQUAL => "@="
-| LEAF COLONEQUAL => ":="
-(**| NODE [] => ""
-| NODE (x :: xs) => to_string (x) ++ to_string (NODE xs)**)
-| _ => ""
-end.
+Definition to_string (t : tree) : string :=
+  match t with
+  | LEAF (NAME n) => n
+  | LEAF (NUMBER n) => n
+  | LEAF (STRING s) => "'" ++ s ++ "'"
+  | LEAF LPAR => "("
+  | LEAF RPAR => ")"
+  | LEAF COLON => ":"
+  | LEAF COMMA => ","
+  | LEAF SEMI => ";"
+  | LEAF PLUS => "+"
+  | LEAF MINUS => "-"
+  | LEAF STAR => "*"
+  | LEAF SLASH => "/"
+  | LEAF AMPER => "&"
+  | LEAF LESS => "<"
+  | LEAF GREATER => ">"
+  | LEAF EQUAL => "="
+  | LEAF DOT => "."
+  | LEAF PERCENT => "%"
+  | LEAF BACKQUOTE => "`"
+  | LEAF LBRACE => "{"
+  | LEAF RBRACE => "}"
+  | LEAF EQEUQAL => "=="
+  | LEAF NOTEQUAL => "!="
+  | LEAF LESSEQUAL => "<="
+  | LEAF GREATEREQUAL => ">="
+  | LEAF TILDE => "~"
+  | LEAF CIRCUMFLEX => "^"
+  | LEAF LEFTSHIFT => "<<"
+  | LEAF RIGHTSHIFT => ">>"
+  | LEAF DOUBLESTAR => "**"
+  | LEAF PLUSEQUAL => "+="
+  | LEAF MINEQUAL => "-="
+  | LEAF STAREQUAL => "*="
+  | LEAF SLASHEQUAL => "/="
+  | LEAF PERCENTEQUAL => "%="
+  | LEAF AMPEREQUAL => "&="
+  | LEAF VBAREQUAL => "|="
+  | LEAF CIRCUMFLEXEQUAL => "^="
+  | LEAF LEFTSHIFTEQUAL => "<<="
+  | LEAF RIGHTSHIFTEQUAL => ">>="
+  | LEAF DOUBLESTAREQUAL => "**="
+  | LEAF DOUBLESLASH => "//"
+  | LEAF DOUBLESLASHEQUAL => "//="
+  | LEAF AT => "@"
+  | LEAF ATEQUAL => "@="
+  | LEAF COLONEQUAL => ":="
+  | _ => ""
+  end.
 
-Definition fix_print (t : tree) : tree :=
-match t with
-| NODE [LEAF (NAME "print") ; n] => NODE [LEAF (NAME "print") ; LEAF LPAR ; n ; LEAF RPAR]
-| NODE [LEAF (NAME "print") ; n; LEAF COMMA] => NODE [LEAF (NAME "print") ; LEAF LPAR ; n ; LEAF COMMA; LEAF (NAME "end") ; LEAF EQUAL ; LEAF (STRING " ") ; LEAF RPAR]
-| NODE [LEAF (NAME "print") ; LEAF RIGHTSHIFT ; LEAF (NAME f) ; LEAF COMMA ; n] => NODE [LEAF (NAME "print") ; LEAF LPAR ; n ; LEAF COMMA; LEAF (NAME "file") ; LEAF EQUAL ; LEAF (NAME f) ; LEAF RPAR]
-| NODE [LEAF (NAME "print") ; LEAF RIGHTSHIFT ; LEAF (NAME f) ; LEAF COMMA ; n; LEAF COMMA] => NODE [LEAF (NAME "print") ; LEAF LPAR ; n ; LEAF COMMA; LEAF (NAME "end") ; LEAF EQUAL ; LEAF (STRING " ") ; LEAF COMMA; LEAF (NAME "file") ; LEAF EQUAL ; LEAF (NAME f) ; LEAF RPAR]
-| _ => t
-end.
+Definition fix_print (l : list tree) : list tree :=
+  match l with
+  | LEAF (NAME "print") :: n => LEAF (NAME "print") :: LEAF LPAR ::
+    (match n with
+    | LEAF RIGHTSHIFT :: LEAF (NAME f) :: LEAF COMMA :: n' => 
+      match rev n' with
+      | LEAF COMMA :: n'' => rev n'' ++ [LEAF COMMA ; LEAF (NAME "end") ; LEAF EQUAL ; LEAF (STRING " ") ; LEAF COMMA; LEAF (NAME "file") ; LEAF EQUAL ; LEAF (NAME f)]
+      | _ => n' ++ [LEAF COMMA; LEAF (NAME "file") ; LEAF EQUAL ; LEAF (NAME f)]
+      end
+    | _ => match rev n with
+      | LEAF COMMA :: n' => rev n' ++ [LEAF COMMA ; LEAF (NAME "end") ; LEAF EQUAL ; LEAF (STRING " ")]
+      | _ => n
+      end
+    end) ++ [LEAF RPAR]
+  | _ => l
+  end.
 
 Definition node_to_list (n : tree) : list tree :=
 match n with
 | NODE l => l
-| _ => [LEAF COMMA]
+| _ => []
 end.
 
-Definition test := NODE [LEAF (NAME "print") ; LEAF RIGHTSHIFT ; LEAF (NAME "test") ; LEAF COMMA ; LEAF (STRING "idk") ; LEAF COMMA].
-Eval simpl in fix_print test.
+Definition transpile (l : list tree) : string := concat_strings (map to_string (fix_print l)).
 
-Eval simpl in concat_strings (map to_string (node_to_list test)).
+Definition test1 := transpile [LEAF (NAME "print")].
+Compute test1.
 
 (**Extract Inductive bool => "bool" [ "true" "false" ].
 Extract Inductive list => "list" [ "[]" "(::)" ].
-Recursive Extraction tree.**)
+Recursive Extraction test1.
 
--- src/tests/tester.py
+++ src/tests/tester.py
@@ -1,27 +1,96 @@
 import subprocess
-import csv
 
 datafile = open("data.csv", "w")
 print(
-    "Test | Original Code Output | 2to3 Translation | 2to3 Output | 2to3 Output Matches Original Output | 2to3 Run Time | fix_print Translation | fix_print Output | fix_print Output Matches Original Output | fix_print Run Time",
+    "Test | Python 2 Output | 2to3 Translation | 2to3 Output | 2to3 Output Matches Original Output | 2to3 Run Time 1 | 2to3 Run Time 2 | 2to3 Run Time 3 | 2to3 Run Time 4 | 2to3 Run Time 5 | fix_print Translation | fix_print Output | fix_print Output Matches Original Output | fix_print Run Time",
     file=datafile,
 )
 
-for i in range(1, 4):
-    two_to_three_test = subprocess.run(
-        ["2to3", f"test{i}.py2"], capture_output=True, encoding="utf-8"
-    )
 
-    two_to_three_test_result = two_to_three_test.stdout.splitlines()
-    for line in two_to_three_test_result:
-        if line.startswith("+"):
-            two_to_three_translation = line.lstrip("+")
+for i in range(1, 12):
+    two_to_three_runtimes = []
+
+    for j in range(5):
+        python_two_output = subprocess.run(
+            ["python2", f"test{i}.py2"], capture_output=True
+        ).stdout
+
+        two_to_three_test = subprocess.run(
+            f"time 2to3 test{i}.py2",
+            shell=True,
+            executable="/bin/bash",
+            capture_output=True,
+            encoding="utf-8",
+        )
+
+        for line in two_to_three_test.stdout.splitlines():
+            if line.startswith("+"):
+                two_to_three_translation = line.lstrip("+")
+
+        for line in two_to_three_test.stderr.splitlines():
+            if line.startswith("real"):
+                two_to_three_runtimes.append(int(line[-4:-1]))
+
+    two_to_three_average_runtime = sum(two_to_three_runtimes) / 5
 
     two_to_three_translation_output = subprocess.run(
         ["python", "-c", two_to_three_translation], capture_output=True
     ).stdout
 
+    python_two_output_equals_two_to_three_output = (
+        python_two_output == two_to_three_translation_output
+    )
+
     print(
-        f"{i} | {two_to_three_translation} | {two_to_three_translation_output}",
+        f"{i} | {python_two_output} | {two_to_three_translation} | {two_to_three_translation_output} | {two_to_three_runtimes[0]} | {two_to_three_runtimes[1]} | {two_to_three_runtimes[2]} | {two_to_three_runtimes[3]} | {two_to_three_runtimes[4]} | {two_to_three_average_runtime} | {python_two_output_equals_two_to_three_output}",
+        file=datafile,
+    )
+
+
+for i in range(1, 7):
+    subprocess.run(["python2", f"filetest{i}.py2"])
+
+    with open(f"filetest{i}.txt", "br") as two_output_file:
+        python_two_output = two_output_file.read()
+
+    two_to_three_runtimes = []
+
+    for j in range(5):
+        two_to_three_test = subprocess.run(
+            f"time 2to3 filetest{i}.py2",
+            shell=True,
+            executable="/bin/bash",
+            capture_output=True,
+            encoding="utf-8",
+        )
+
+        for line in two_to_three_test.stdout.splitlines():
+            if line.startswith("+"):
+                two_to_three_translation = line.lstrip("+")
+
+        for line in two_to_three_test.stderr.splitlines():
+            if line.startswith("real"):
+                two_to_three_runtimes.append(int(line[-4:-1]))
+
+    two_to_three_average_runtime = sum(two_to_three_runtimes) / 5
+
+    subprocess.run(
+        [
+            "python",
+            "-c",
+            'x = open("filetest1.txt", "w")' + "\n" + two_to_three_translation,
+        ],
+        capture_output=True,
+    )
+
+    with open(f"filetest{i}.txt", "br") as two_output_file:
+        two_to_three_translation_output = two_output_file.read()
+
+    python_two_output_equals_two_to_three_output = (
+        python_two_output == two_to_three_translation_output
+    )
+
+    print(
+        f"Filetest {i} | {python_two_output} | {two_to_three_translation} | {two_to_three_translation_output} | {two_to_three_runtimes[0]} | {two_to_three_runtimes[1]} | {two_to_three_runtimes[2]} | {two_to_three_runtimes[3]} | {two_to_three_runtimes[4]} | {two_to_three_average_runtime} | {python_two_output_equals_two_to_three_output}",
         file=datafile,
     )
